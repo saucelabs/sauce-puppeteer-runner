08866c3a69924929a87c668f190309689114e1e2
christian-bromann <mail@christian-bromann.com>
2020-02-04 11:29:14 -0800

    make upload updated log file

--- jest.setup.js
+++ jest.setup.js
@@ -1,25 +1,85 @@
+const fs = require('fs')
 const puppeteer = require('puppeteer')
+const debug = require('debug')
+const ansiRegex = require('ansi-regex')
 const { remote } = require('webdriverio')
 
-// ;(async () => {
-//   const client = await remote({
-//     user: process.env.SAUCE_USERNAME,
-//     key: process.env.SAUCE_ACCESS_KEY,
-//     logLevel: 'error',
-//     capabilities: {
-//       browserName: 'chrome',
-//       browserVersion: 'latest',
-//       platformName: 'Windows 10',
-//       'sauce:options': {
-//         name: 'Puppeteer Test'
-//       }
-//     }
-//   })
-//   return client.deleteSession()
-// })().then(console.log, console.error)
+global.logs = []
+global.isDone = false
+
+jest.setTimeout(20000)
 
 beforeAll(async () => {
+  global.session = await remote({
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    logLevel: 'error',
+    capabilities: {
+      browserName: 'chrome',
+      browserVersion: 'latest',
+      platformName: 'Windows 10',
+      'sauce:options': {
+        name: 'Puppeteer Test'
+      }
+    }
+  })
+  global.sessionId = global.session.sessionId
+  console.log('Session started', global.sessionId)
+
   global.browser = await puppeteer.launch({
     headless: false
   })
+
+  let lastCommand = Date.now()
+  debug.log = (...args) => {
+    const sendString = 'SEND ► '
+    const receiveString = '◀ RECV'
+
+    if (args[0].includes(receiveString)) {
+      const line = args[0].slice(args[0].indexOf('{'))
+
+      try {
+        const response = JSON.parse(line)
+        if (!response.id) {
+          return
+        }
+
+        const log = global.logs.find(
+          (log) => parseInt(log.id, 10) === parseInt(response.id))
+
+        if (log) {
+          log.result = response
+        }
+      } catch (e) {
+        console.log(e)
+      }
+
+      return
+    }
+
+    if (!args[0].includes(sendString)) {
+      return
+    }
+
+    const line = args[0].slice(args[0].indexOf(sendString) + sendString.length)
+    const command = JSON.parse(line)
+    const duration = (Date.now() - lastCommand) / 1000
+    global.logs.push({
+      id: command.id,
+      screenshot: 0,
+      between_commands: duration,
+      start_time: Date.now() / 1000,
+      suggestion_values: [],
+      request: command.params,
+      HTTPStatus: 200,
+      result: "",
+      suggestion: null,
+      duration,
+      path: command.method,
+      in_video_timeline: 0,
+      method: "SOCKET",
+      statusCode: 0
+    })
+    lastCommand = Date.now()
+  }
 })
--- jest.teardown.js
+++ jest.teardown.js
@@ -1,3 +1,27 @@
+const { promisify } = require('util')
+const { S3 } = require('aws-sdk')
+
 afterAll(async () => {
+  global.isDone = true
   await global.browser.close()
+  await global.session.deleteSession()
+
+  const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  })
+
+  const params = {
+    ACL: 'public-read',
+    ContentType: 'application/json',
+    Bucket: 'sauce-userdata-us-west-2',
+    Key: `cb-onboarding/${global.sessionId}/log.json`,
+    ServerSideEncryption: 'aws:kms',
+    SSEKMSKeyId: process.env.AWS_SSEKMS_KEY_ID,
+    Body: JSON.stringify(global.logs, null, 4)
+  };
+
+  console.log(`Update log file: cb-onboarding/${global.sessionId}/log.json`);
+  await promisify(s3.upload.bind(s3))(params)
+  console.log('Done!')
 });
