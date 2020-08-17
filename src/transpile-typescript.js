37bc7249a837fb78cc933302d9c71896daec4b94
Dan <dpgraham.85@gmail.com>
2020-08-17 09:42:50 -0700

    Add typescript support

---
+++ src/transpile-typescript.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+let { exec } = require('child_process');
+const exists = promisify(fs.exists);
+exec = promisify(exec);
+
+(async function () {
+    const tsconfigPath = path.join(path.join(process.env.HOME_DIR, 'tests', 'tsconfig.json'));
+    let res;
+    if (await exists(tsconfigPath)) {
+      console.log(`Compiling Typescript files from tsconfig '${tsconfigPath}'`);
+      res = await exec(`npx tsc -p ${tsconfigPath}`);
+      if (res.stderr) {
+          throw res.stderr;
+      }
+    }
+})().catch((e) => {
+    console.error(`Error: '${e}'`);
+    process.exit(1);
+});
