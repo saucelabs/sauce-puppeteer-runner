0be7b4479765f177cea33db793bdb052f1b1462e
Alex Plischke <alex.plischke@saucelabs.com>
2020-09-08 15:08:06 -0700

    Add dynamic test matching

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -1,6 +1,40 @@
 const path = require('path')
+const fs = require('fs');
+const yaml = require('js-yaml');
+const {HOME_DIR} = require('../src/constants')
 
-const { HOME_DIR } = require('../src/constants')
+// the default test matching behavior for versions <= v0.1.4
+const DefaultRunCfg = {
+    projectPath: "${HOME_DIR}",
+    match: [
+        `${HOME_DIR}/tests/?(*.)+(spec|test).[jt]s?(x)`,
+        `${HOME_DIR}/tests/**/?(*.)+(spec|test).[jt]s?(x)`
+    ]
+}
+
+const runCfgPath = path.join(HOME_DIR, 'run.yaml')
+runCfg = loadRunConfig(runCfgPath)
+testMatch = resolveTestMatches(runCfg)
+
+function loadRunConfig(cfgPath) {
+    if (fs.existsSync(cfgPath)) {
+        return yaml.safeLoad(fs.readFileSync(cfgPath, 'utf8'));
+    }
+
+    return DefaultRunCfg
+}
+
+function resolveTestMatches(runCfg) {
+    return runCfg.match.map(
+        p => {
+            if (path.isAbsolute(p)) {
+                return p
+            }
+            return path.join(runCfg.projectPath, p)
+        }
+    );
+}
+
 
 module.exports = {
     rootDir: HOME_DIR,
@@ -13,8 +47,5 @@ module.exports = {
         `default`,
         `${HOME_DIR}/src/reporter.js`
     ],
-    testMatch: [
-        `${HOME_DIR}/tests/?(*.)+(spec|test).[jt]s?(x)`,
-        `${HOME_DIR}/tests/**/?(*.)+(spec|test).[jt]s?(x)`
-    ]
+    testMatch: testMatch,
 };
